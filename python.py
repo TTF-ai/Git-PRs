ad30b39a1ef458049e158095edc81488479078d3
TTF-ai <thirumaleshk549@gmail.com>
2026-03-27 12:13:06 +0530

    fix(python): [AST003] `print()` found — consider using logging instead
    Auto-generated by CleanCodeX on line 4.
    Linting rule: AST003

--- python.py
+++ python.py
@@ -1,7 +1,11 @@
+import logging
 first_number = 10
 second_number = 5
 
-print(f"Addition: {first_number + second_number}")
+logging.info(f"Addition: {first_number + second_number}")
 
-print(f"Subtraction: {first_number - second_number}")
-print(f"Multiplication: {first_number * second_number}")
+logging.info(f"Subtraction: {first_number - second_number}")
+logging.info(f"Multiplication: {first_number * second_number}")
+abs
+abs
+logging.info('hello world')
